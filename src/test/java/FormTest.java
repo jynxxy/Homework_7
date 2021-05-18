6fdcbb2edfdb75be93dd23a1b4c8ad5ec422abba
spiesko <spiesko@sii.pl>
2021-05-18 13:15:23 +0200

    logback added

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Random;
@@ -11,35 +13,72 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class FormTest extends TestBase {
 
+    private Logger logger = LoggerFactory.getLogger("FormTest.class");
+
     Random random = new Random();
+    String url;
+    String firstName;
+    String lastName;
+    String email;
+    String age;
+    String continent;
+    int seleniumCommandIndex;
+    String filePath;
 
     @Test
     public void shouldFillFormWithSuccess() {
+        url = "https://seleniumui.moderntester.pl/form.php";
+        getDriver().get(url);
+        logger.info("Webpage url: " + url);
 
-        getDriver().get("https://seleniumui.moderntester.pl/form.php");
-        getDriver().findElement(By.id("inputFirstName3")).sendKeys("Jan");
-        getDriver().findElement(By.id("inputLastName3")).sendKeys("Kowalski");
-        getDriver().findElement(By.id("inputEmail3")).sendKeys("jankowalski@sii.pl");
+        firstName = "Jan";
+        lastName = "Kowalski";
+        getDriver().findElement(By.id("inputFirstName3")).sendKeys(firstName);
+        getDriver().findElement(By.id("inputLastName3")).sendKeys(lastName);
+        logger.debug("First name is: {} and last name is: {}", firstName, lastName);
+
+        email = "jankowalski@sii.pl";
+        getDriver().findElement(By.id("inputEmail3")).sendKeys(email);
+        logger.debug("Email is: {}", email);
+
         getDriver().findElements(By.cssSelector("[name='gridRadiosSex']")).get(0).click();
-        getDriver().findElement(By.id("inputAge3")).sendKeys("33");
-        List<WebElement> gridRadioExperience = getDriver().findElements(By.cssSelector(".form-group [name='gridRadiosExperience']"));
+        logger.debug("Radio button properly clicked");
+        age = "33";
+        getDriver().findElement(By.id("inputAge3")).sendKeys(age);
+        logger.debug("Age is: {}", age);
+
+        List<WebElement> gridRadioExperience = getDriver().
+                findElements(By.cssSelector(".form-group [name='gridRadiosExperience']"));
+        logger.debug("Number of radios: " + gridRadioExperience.size());
         int index = random.nextInt(gridRadioExperience.size());
         if (!gridRadioExperience.get(index).isSelected()) {
             gridRadioExperience.get(index).click();
         }
+        logger.debug("Random index of radio: " + index);
 
         getDriver().findElements(By.xpath("//*[@name='gridCheckboxProfession']")).get(0).click();
+        logger.debug("Checkbox button properly clicked");
 
+        continent = "europe";
         Select continents = new Select(getDriver().findElement(By.id("selectContinents")));
-        continents.selectByValue("europe");
+        continents.selectByValue(continent);
+        logger.debug("Selected continent: " + continent);
+
+        seleniumCommandIndex = 4;
         Select selectSeleniumCommands = new Select(getDriver().findElement(By.id("selectSeleniumCommands")));
-        selectSeleniumCommands.selectByValue("wait-commands");
-        
-        getDriver().findElement(By.cssSelector("#chooseFile")).sendKeys(System.getProperty("user.dir")+"/src/main/resources/test.txt");
+        selectSeleniumCommands.selectByIndex(seleniumCommandIndex);
+        logger.debug("Selected value: " + seleniumCommandIndex + ", options: " + selectSeleniumCommands.getOptions());
+
+        filePath = System.getProperty("user.dir")+"/src/main/resources/test.txt";
+        getDriver().findElement(By.cssSelector("#chooseFile")).sendKeys(filePath);
+        logger.debug("File path is :" + filePath);
 
         getDriver().findElement(By.cssSelector(".btn-primary")).click();
+        logger.debug("Button properly clicked");
+
         //this should be at the end of test
         WebElement msg = getDriver().findElement(By.id("validator-message"));
+        logger.debug("Message: " + msg.getText());
         assertThat(msg.getText(), equalTo("Form send with success"));
     }
 
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,29 +1,36 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestBase {
 
-    private WebDriver driver;
+    private static Logger logger = LoggerFactory.getLogger("TestBase.java");
+
+    private static WebDriver driver;
 
     public WebDriver getDriver() {
         return driver;
     }
 
-    @BeforeEach
-    public void setUp() {
+    @BeforeAll
+    static void setUp() {
         ChromeOptions options = new ChromeOptions();
         WebDriverManager.chromedriver().setup();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
+        logger.info(">>>>> Starting test <<<<<");
     }
 
-    @AfterEach
-    public void cleanUp() {
+    @AfterAll
+    static void tearDown() {
         driver.quit();
+        logger.info("Driver has been closed properly");
+        logger.info(">>>>> Finished test <<<<<");
     }
 
 }
